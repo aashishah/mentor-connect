4b3ef7b0f68a262f023738825faabceff1f7dabc
shruti <shrutigupta5555@gmail.com>
2021-04-25 08:05:39 +0530

    ADD: firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCcmSjZ0RrfRZmi8uqIwr9bThtE1cQWlak",
+    authDomain: "mentor-connect-b27bb.firebaseapp.com",
+    projectId: "mentor-connect-b27bb",
+    storageBucket: "mentor-connect-b27bb.appspot.com",
+    messagingSenderId: "730141276763",
+    appId: "1:730141276763:web:a5b94df05202d0e5515d7e"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
